8a7b59d4eea57278ed328649cadb55e4b83c0fec
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-17 13:34:27 +0200

    handle wrong format and output pair

--- src/main/kotlin/logic/use_case/GetByDate.kt
+++ src/main/kotlin/logic/use_case/GetByDate.kt
@@ -1,15 +1,25 @@
 package logic.use_case
 
 import logic.MealsRepository
-import model.Meal
-import java.util.Date
+import java.text.ParseException
+import java.text.SimpleDateFormat
 
 class GetByDate(
     private val mealsRepository: MealsRepository,
-    private val date: Date
 ) {
-    fun getByDate(): List<Meal> {
-        return mealsRepository.getAllMeals()
-            .filter { it.submitted == date }
+    fun getByDate(date: String): Result<List<Pair<Int, String>>> {
+        try {
+            val dateParsed = SimpleDateFormat("dd-mm-yyyy").parse(date)
+            val meals = mealsRepository.getAllMeals()
+            val filtered = meals.filter { it.submitted == dateParsed }
+            return if (filtered.isEmpty())
+                Result.failure(Exception("No meals found for date: $date"))
+            else
+                Result.success(filtered.map { it.let { Pair(it.id, it.name) } })
+        } catch (e: ParseException) {
+            return Result.failure(Exception("Invalid date format: $date. Expected format: dd-mm-yyyy"))
+        } catch (e: Exception) {
+            return Result.failure(Exception("An error happened: ${e.message}"))
+        }
     }
 }
