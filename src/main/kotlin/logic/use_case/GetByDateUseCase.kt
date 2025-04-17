b60f919b5d4966b7dc6e6eb4afa590aac9cdd4d4
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-17 18:48:51 +0200

    validate date format

--- src/main/kotlin/logic/use_case/GetByDateUseCase.kt
+++ src/main/kotlin/logic/use_case/GetByDateUseCase.kt
@@ -9,18 +9,17 @@ class GetByDateUseCase(
     private val mealsRepository: MealsRepository,
 ) {
     fun getByDate(date: String): Result<List<Meal>> {
-        try {
-            val dateParsed = SimpleDateFormat(Meal.DATE_FORMAT).parse(date)
-            val meals = mealsRepository.getAllMeals()
-            val filtered = meals.filter { it.submitted == dateParsed }
-            return if (filtered.isEmpty())
-                Result.failure(Exception("No meals found for date: $date"))
-            else
-                Result.success(filtered)
-        } catch (e: ParseException) {
+        if (date.isEmpty())
+            return Result.failure(Exception("Date cannot be empty"))
+        if (!date.trim().matches(Regex("(0?[1-9]|[1-2][0-9]|3[01])-(0?[1-9]|1[0-2])-[1-2]\\d{3}")))
             return Result.failure(Exception("Invalid date format: $date. Expected format: ${Meal.DATE_FORMAT}"))
-        } catch (e: Exception) {
-            return Result.failure(Exception("An error happened: ${e.message}"))
-        }
+
+        val dateParsed = SimpleDateFormat(Meal.DATE_FORMAT).parse(date)
+        val meals = mealsRepository.getAllMeals()
+        val filtered = meals.filter { it.submitted == dateParsed }
+        return if (filtered.isEmpty())
+            Result.failure(Exception("No meals found for date: $date"))
+        else
+            Result.success(filtered)
     }
 }
