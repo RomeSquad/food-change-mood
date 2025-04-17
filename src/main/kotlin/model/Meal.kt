b8c4e913b29c2a241192aecfaf5ec0d689fc005f
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-17 14:58:03 +0200

    override toString

--- src/main/kotlin/model/Meal.kt
+++ src/main/kotlin/model/Meal.kt
@@ -1,5 +1,6 @@
 package model
 
+import java.text.SimpleDateFormat
 import java.util.Date
 
 data class Meal(
@@ -15,4 +16,21 @@ data class Meal(
     val description: String?,
     val ingredients: List<String>,
     val nIngredients: Int,
-)
+) {
+    override fun toString(): String {
+        return "Id=$id      Name='$name'\n" +
+                "Time to prepare=$minutes \n" +
+                "description=$description \n" +
+                "$nutrition \n" +
+                "consists of $nIngredients ingredients " +
+                "ingredients: $ingredients  " +
+                "How to prepare =$nSteps steps=$steps,\n" +
+                "Shared by contributor $contributorId on ${submitted.date}-${submitted.month}-${submitted.year}\n" +
+                "tags=$tags \n"
+
+    }
+
+    companion object{
+        const val DATE_FORMAT = "dd-mm-yyyy"
+    }
+}
