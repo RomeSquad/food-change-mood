7668f5903ed651b4bc160099fb3b96a3e58670cc
Safa Thaaer <safathaaer@gmail.com>
2025-04-18 11:03:55 +0300

    rename class to HealthyMealsFilterUseCase and separation of quartile function to private fun

--- src/main/kotlin/logic/use_case/HealthyMealsFilterUseCase.kt
+++ src/main/kotlin/logic/use_case/HealthyMealsFilterUseCase.kt
@@ -1,27 +1,42 @@
 package logic.use_case
+
 import model.Meal
 
-class HealthyMealsFilter (){
+class HealthyMealsFilter {
+
     fun getHealthyFastMeals(meals: List<Meal>): List<Meal> {
+
         val totalFats = meals.map { it.nutrition.totalFat }
+
         val saturatedFats = meals.map { it.nutrition.saturatedFat }
+
         val carbohydrates = meals.map { it.nutrition.carbohydrates }
-        fun quartile(values: List<Double>): Double {
-            return values.sorted()[values.size / 4]
-        }
+
+
         val fatThreshold = quartile(totalFats)
+
         val saturatedThreshold = quartile(saturatedFats)
+
         val carbsThreshold = quartile(carbohydrates)
 
+
         return meals.filter {
+
             it.minutes <= 15 &&
+
                     it.nutrition.totalFat <= fatThreshold &&
+
                     it.nutrition.saturatedFat <= saturatedThreshold &&
+
                     it.nutrition.carbohydrates <= carbsThreshold
         }
     }
 }
+private fun quartile(values: List<Double>): Double {
 
+    return values.sorted()[values.size / 4]
+
+}
 
 
 
