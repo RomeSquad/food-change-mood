c5e7d9c7d6c0547dd7cb2fded5ef40aa28285086
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-04-22 15:27:39 +0200

    fix date regex

--- src/main/kotlin/domain/use_case/GetByDateUseCase.kt
+++ src/main/kotlin/domain/use_case/GetByDateUseCase.kt
@@ -12,7 +12,7 @@ class GetByDateUseCase(
         if (date.isEmpty())
             throw Exception("Date cannot be empty")
 
-        if (!date.trim().matches(Regex("\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])")))
+        if (!date.trim().matches(Regex("\\d{4}-(0?[1-9]|1[0-2])-(0?[1-9]|[12]\\d|3[01])")))
             throw Exception("Invalid date format: $date. Expected format: ${Meal.DATE_FORMAT}")
 
         val dateParsed = SimpleDateFormat(Meal.DATE_FORMAT).parse(date)
