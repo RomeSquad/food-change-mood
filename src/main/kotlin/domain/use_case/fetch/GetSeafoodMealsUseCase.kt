51ad90f73cc9238231ee0cfd80c1a362da7c525a
Abanoub Nagy <114380262+Abanoub-Nagy@users.noreply.github.com>
2025-04-25 18:31:24 +0300

    check for no seafood meals

--- src/main/kotlin/domain/use_case/fetch/GetSeafoodMealsUseCase.kt
+++ src/main/kotlin/domain/use_case/fetch/GetSeafoodMealsUseCase.kt
@@ -3,27 +3,35 @@ package domain.use_case.fetch
 import data.meal.MealsRepository
 import data.model.Meal
 import data.model.seafood_meals.SeafoodMeal
+import domain.NoMealsFoundException
 
 class GetSeafoodMealsUseCase(
     private val mealsRepository: MealsRepository
 ) {
-    fun getSeafoodMeals(): List<SeafoodMeal> =
-        mealsRepository.getAllMeals()
-            .filter(::onlyContainSeafoodMeal)
-            .takeIf { it.isNotEmpty() }
-            ?.sortedByDescending { it.nutrition.protein }
-            ?.map {
-                SeafoodMeal(
-                    name = it.name,
-                    protein = it.nutrition.protein
-                )
-            }
-            ?: throw NoSuchElementException("No seafood meals based on protein found")
 
-    private fun onlyContainSeafoodMeal(meal: Meal): Boolean =
-        meal.tags.any { it.contains(TAG_NAME, ignoreCase = true) }
+    fun getSeafoodMeals(): List<SeafoodMeal> {
+        val seafoodMeals = mealsRepository.getAllMeals()
+            .filter(::isSeafoodMeal)
+            .sortedByDescending { it.nutrition.protein }
+
+        if (seafoodMeals.isEmpty()) {
+            throw NoMealsFoundException("No seafood meals found")
+        }
+
+        return seafoodMeals.mapIndexed { index, meal ->
+            SeafoodMeal(
+                name = meal.name,
+                protein = meal.nutrition.protein
+            )
+        }
+
+    }
+
+    private fun isSeafoodMeal(meal: Meal): Boolean {
+        return meal.tags.any { it.contains(TAG_NAME, ignoreCase = true) }
+    }
 
     companion object {
-        const val TAG_NAME = "seafood"
+        private const val TAG_NAME = "seafood"
     }
 }
