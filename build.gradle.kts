d95651280cd61e320ff8adca7469641318de3e77
Mohamed_Dev <mohamedzarea29@gmail.com>
2025-04-26 17:33:31 +0300

    add jacoco plugin and GitHub workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    jacoco
 }
 
 group = "org.example"
@@ -10,26 +11,70 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
     implementation("io.insert-koin:koin-core:4.0.2")
 
+    testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
-
-    // Google Truth
     testImplementation("com.google.truth:truth:1.4.2")
-
-    // parameterized test
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
-
-    // mock K
     testImplementation("io.mockk:mockk:1.14.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-kotlin {
-    jvmToolchain(17)
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        html.required.set(true)
+    }
+
+    classDirectories.setFrom(
+        files(
+            classDirectories.files.map {
+                fileTree(it) {
+                    exclude(
+                        "**/model/**",
+                        "**/di/**"
+                    )
+                }
+            }
+        )
+    )
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.test)
+
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.30".toBigDecimal() // 100% coverage required
+            }
+        }
+    }
+
+    classDirectories.setFrom(
+        files(
+            fileTree("build/classes/kotlin/main") {
+                exclude("**/model/**", "**/di/**")
+            }
+        )
+    )
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
